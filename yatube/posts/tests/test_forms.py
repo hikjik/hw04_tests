c6634cbd907cc19b1ce48624f4de03bf244ff8b3
Александр Морев <a.morev@kryptonite.ru>
2022-05-08 18:43:14 +0300

    Update posts.test_forms

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,4 +1,6 @@
+from http import HTTPStatus
 from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -11,41 +13,46 @@ class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create_user(username="user")
+        cls.post_author = User.objects.create_user(username="user")
         cls.group = Group.objects.create(
             title="title",
             slug="slug",
             description="description",
         )
         cls.post = Post.objects.create(
-            author=cls.user,
+            author=cls.post_author,
             group=cls.group,
             text="text",
         )
 
     def setUp(self):
+        self.guest_client = Client()
+
+        self.user = User.objects.create_user(username="auth user")
         self.auth_client = Client()
-        self.auth_client.force_login(PostFormTests.user)
+        self.auth_client.force_login(self.user)
 
-    def test_create_post(self):
+        self.post_author_client = Client()
+        self.post_author_client.force_login(PostFormTests.post_author)
+
+    def test_create_post_authorized_client(self):
         posts_count = Post.objects.count()
-        user = PostFormTests.user
         group = PostFormTests.group
-
         form_data = {
             "text": "Тестовый текст",
             "group": group.id,
         }
+
         response = self.auth_client.post(
             reverse("posts:post_create"),
             data=form_data,
             follow=True,
         )
+
         self.assertRedirects(
             response,
-            reverse("posts:profile", kwargs={"username": user.username}),
+            reverse("posts:profile", kwargs={"username": self.user.username}),
         )
-
         self.assertEqual(Post.objects.count(), posts_count + 1)
         self.assertTrue(
             Post.objects.filter(
@@ -54,15 +61,29 @@ class PostFormTests(TestCase):
             ).exists()
         )
 
-    def test_edit_post(self):
+    def test_create_post_redirect_anonymous(self):
+        form_data = {
+            "text": "Тестовый текст",
+            "group": PostFormTests.group.id,
+        }
+        path = reverse("posts:post_create")
+        response = self.guest_client.post(path, data=form_data, follow=True)
+
+        self.assertRedirects(
+            response,
+            reverse("users:login") + "?next=" + path,
+            status_code=302,
+            target_status_code=200,
+        )
+
+    def test_edit_post_author_client(self):
         posts_count = Post.objects.count()
         post = PostFormTests.post
-
         form_data = {
             "text": "Новый тестовый текст",
             "group": post.group.id,
         }
-        response = self.auth_client.post(
+        response = self.post_author_client.post(
             reverse("posts:post_edit", kwargs={"post_id": post.id}),
             data=form_data,
             follow=True,
@@ -73,6 +94,37 @@ class PostFormTests(TestCase):
             reverse("posts:post_detail", kwargs={"post_id": post.id}),
         )
         self.assertEqual(Post.objects.count(), posts_count)
-        self.assertTrue(
-            Post.objects.get(id=post.id).text, form_data["text"]
+        self.assertTrue(post.text, form_data["text"])
+
+    def test_edit_post_redirect_anonymous(self):
+        post = PostFormTests.post
+        form_data = {
+            "text": "Новый тестовый текст",
+            "group": post.group.id,
+        }
+        path = reverse("posts:post_edit", kwargs={"post_id": post.id})
+        response = self.guest_client.post(path, data=form_data, follow=True)
+
+        self.assertRedirects(
+            response,
+            reverse("users:login") + "?next=" + path,
+            status_code=302,
+            target_status_code=200,
+        )
+
+    def test_edit_post_redirect_authorized_not_author(self):
+        post = PostFormTests.post
+        form_data = {
+            "text": "Новый тестовый текст",
+            "group": post.group.id,
+        }
+
+        path = reverse("posts:post_edit", kwargs={"post_id": post.id})
+        response = self.auth_client.post(path, data=form_data, follow=True)
+
+        self.assertRedirects(
+            response,
+            reverse("posts:post_detail", kwargs={"post_id": post.id}),
+            status_code=302,
+            target_status_code=200,
         )
